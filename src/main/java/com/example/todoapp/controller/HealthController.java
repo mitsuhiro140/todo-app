7a53e451f443482ec11c011f9afceefa6327b011
mitsuhiro140 <mitsu14023@gmail.com>
2024-10-09 16:19:41 +0900

    first commit

---
+++ src/main/java/com/example/todoapp/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.todoapp.controller;
+
+import com.example.todoapi.controller.HealthApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController implements HealthApi {
+    @Override
+    public ResponseEntity<Void> healthGet() {
+        return ResponseEntity.ok().build();
+    }
+}
